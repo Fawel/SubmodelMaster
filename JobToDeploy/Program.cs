8b4449430d933a884c772ff5d9cf322917c808c6
Fawel <uiasop@gmail.com>
2020-04-25 19:07:21 +0300

    Added console app for fun

---
+++ JobToDeploy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobToDeploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, we have some work here!");
+        }
+    }
+}
